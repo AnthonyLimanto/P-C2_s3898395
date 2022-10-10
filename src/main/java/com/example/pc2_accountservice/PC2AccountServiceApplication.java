0343b14c4c68bdbc0358ec4f1ff9a18b7b9539e6
anthonyLimanto <s3898395@student.rmit.edu.au>
2022-10-11 10:42:10 +1100

    first commit

---
+++ src/main/java/com/example/pc2_accountservice/PC2AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pc2_accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PC2AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PC2AccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pc2_accountservice/PC2AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pc2_accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PC2AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
