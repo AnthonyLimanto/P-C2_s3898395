e3f6da0903b613048b68c4afd781b4b926d34de4
anthonyLimanto <s3898395@student.rmit.edu.au>
2022-10-11 11:07:43 +1100

    initial commit for person service

---
+++ Person_Service/P-C2_PersonService/Person_Service/src/main/java/com/example/pc2_personservice/PC2PersonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pc2_personservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PC2PersonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PC2PersonServiceApplication.class, args);
+    }
+
+}
---
+++ Person_Service/P-C2_PersonService/Person_Service/src/test/java/com/example/pc2_personservice/PC2PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pc2_personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PC2PersonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
