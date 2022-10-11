5b98a3d8dfeb08ad4dc540d201846fb5933523ae
anthonyLimanto <s3898395@student.rmit.edu.au>
2022-10-11 22:35:38 +1100

    fixed the tests, also now when creating table it makes a fresh table, and drops when app close, this is just to ensure test are consistent

--- Account_Service/src/test/java/com/example/pc2_accountservice/AccountTest.java
+++ Account_Service/src/test/java/com/example/pc2_accountservice/AccountTest.java
@@ -26,8 +26,8 @@ public class AccountTest {
 
     @BeforeEach
     void setUp() throws Exception {
-        test_account_1 = "{\"account_name\":\"J_Smith\",\"acc_num\":\"23456789\",\"type\":\"Loan\",\"balance\": \"300\", \"date\" : \"2022-10-11\"}";
-        test_account_2 = "{\"account_name\":\"Anthony Limanto\",\"acc_num\":\"1123456789\",\"type\":\"Term_Investment\",\"balance\": \"800\", \"date\" : \"2022-10-11\"}";
+        test_account_1 = "{\"id\":1,\"type\":\"Loan\",\"acc_num\":\"23456789\",\"account_name\":\"J_Smith\",\"balance\":300.0,\"date\":\"2022-10-11T00:00:00.000+00:00\"}";
+        test_account_2 = "{\"id\":2,\"type\":\"Term_Investment\",\"acc_num\":\"1123456789\",\"account_name\":\"Anthony Limanto\",\"balance\":800.0,\"date\":\"2022-10-11T00:00:00.000+00:00\"}";
 
         // Add the doctors to the JPA
         this.mockMvc.perform(MockMvcRequestBuilders
@@ -43,28 +43,23 @@ public class AccountTest {
     }
 
     @Test
-    void getAllAccountRequest() throws Exception {
-        // Expect both accounts to be returned
-        String expected_string = "[" + test_account_1 + "," + test_account_2 + "]";
-
-        // get all request on endpoint
+    void deleteAccountRequest() throws Exception {
         request = this.mockMvc.perform(MockMvcRequestBuilders
-                        .get("/accounts/account")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        // Delete request on the mock
+                        .delete("/account/account2")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                // Return the content of the request
                 .andReturn();
-
-        // JSON of what is returned
         content = request.getResponse().getContentAsString();
-        assertEquals(content, expected_string);
+        // Assert that the request has gone through and nothing is displayed
+        assertEquals("", content);
     }
 
     @Test
     void addingAccountToDBRequest() throws Exception {
+
         // New doctor to add
-        final String new_Account = "{\"account_name\":\"Abe Paul\",\"acc_num\":\"14545489\",\"type\":\"Term_Investment\",\"balance\": \"8010\", \"date\" : \"2022-10-11\"}";
+        final String new_Account = "{\"id\":3,\"type\":\"Term_Investment\",\"acc_num\":\"14545489\",\"account_name\":\"Abe Paul\",\"balance\":8010.0,\"date\":\"2022-10-11T00:00:00.000+00:00\"}";
         request = this.mockMvc.perform(MockMvcRequestBuilders
                         // Post request on the endpoint
                         .post("/account")
@@ -76,7 +71,7 @@ public class AccountTest {
         content = request.getResponse().getContentAsString();
 
         // Assert that the returned doctor is the new doctor
-        assertEquals(content, new_Account);
+        assertEquals(new_Account, content);
     }
 
     @Test
